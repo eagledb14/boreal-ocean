896779115fd3c91e0ed9d96fc759f0a6c3381240
eagledb14 <eagledb14@gmail.com>
2023-05-24 17:43:19 -0600

    switched to using clag crate and added file reading

--- network-tap/src/file_input.rs
+++ network-tap/src/file_input.rs
@@ -2,8 +2,10 @@ use crate::connection::Connection;
 use crate::network_parsing::parse_traffic;
 
 use std::io::stdin;
+use std::fs;
 
 pub fn read_stdin() -> Vec<Connection> {
+    dbg!("reading from stdin");
     let stdin = stdin();
     let mut connections = Vec::<Connection>::new();
 
@@ -17,3 +19,26 @@ pub fn read_stdin() -> Vec<Connection> {
 
     return connections;
 }
+
+
+pub fn read_file(file_string: &str) -> Vec<Connection> {
+    dbg!("reading from file");
+    let file = match fs::read_to_string(file_string) {
+        Ok(file) => file,
+        Err(e) => {
+            dbg!(e);
+            return Vec::new();
+        },
+    };
+    let mut connections = Vec::<Connection>::new();
+
+    for param in file.lines() {
+        dbg!(&param);
+        if let Some(new_connection) = parse_traffic(&param) {
+            connections.push(new_connection);
+        }
+    }   
+
+    return connections;
+}
+
--- network-tap/src/main.rs
+++ network-tap/src/main.rs
@@ -2,35 +2,44 @@ mod network_parsing;
 mod file_input;
 mod connection;
 
-use std::env::args;
+use clap::Parser;
 
-
-use file_input::*;
+use file_input::{read_stdin, read_file};
 use connection::Connection;
 
 
 // TODO
 // make it more cli friendly
+
 //// add flags for reading from a file, like it is now
-//// add flags to read from stdin
 //// add flags to read directly from tcpdump, you have to include the interface and flags, or not
 ////// give them the flags option
+
 //// for zeek, if you ever figure out how to use that
 //// add option to only collect from a certain ip
 //// option to group by input and print the destinations
+
+
+
+// DONE
+//// add flags to read from stdin
+
 fn main() {
 
-    //if there are not args, then read from stdin
-    if args().len() <= 1 {
-        let connections = read_stdin();
-        print_connections(&connections);
-        return;
+    let cli = Cli::parse();
+    // println!("{:?}", cli.files);
+    let mut connections = Vec::<Connection>::new();
+
+    if let Some(files) = cli.files {
+        for file in files {
+            connections.append(&mut read_file(&file));
+        }
+    }
+    else {
+        connections.append(&mut read_stdin());
     }
 
-
-    for arg in args() {
-        println!("{}", arg);
-    }
+    print_connections(&connections);
 }
 
 fn print_connections(connections: &[Connection]) {
@@ -38,3 +47,17 @@ fn print_connections(connections: &[Connection]) {
         println!("{}\n", connection);
     }
 }
+
+#[derive(Parser, Debug)]
+struct Cli {
+
+    #[arg(short, long, conflicts_with="tcpdump")]
+    files: Option<Vec<String>>,
+
+    #[arg(short = 'x', long)]
+    sort: bool,
+
+    #[arg(short, long, conflicts_with="files")]
+    tcpdump: Option<String>
+}
+
