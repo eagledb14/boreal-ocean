e7dc343d3372c8e1a389b8abfbd13ebff9c105d0
eagledb14 <eagledb14@gmail.com>
2023-05-24 00:37:30 -0600

    reads in pcap strings from tcpdump and returns the time, source, and destination of the traffic

--- network-tap/src/main.rs
+++ network-tap/src/main.rs
@@ -1,94 +1,127 @@
-use std::fs;
-use chrono::{NaiveTime, Local};
-use std::net::IpAddr;
+use std::{fs, fmt};
+use chrono::NaiveTime;
+use std::net::{SocketAddr, IpAddr};
+use std::str::FromStr;
 
 
 fn main() {
-    let in_string = fs::read_to_string("tcpdump-test-doc.txt").expect("couldn't find file");
+
+    let in_string = fs::read_to_string("input-analysis.txt").expect("couldn't find file");
     let params = in_string.trim().lines();
+    let mut traffic = Vec::<Connection>::new();
 
     for param in params {
-        let mut parts = param.split(" ").into_iter();
 
-        //get timestamp
-        let timestamp = if let Some(time) = parts.next() {
-            NaiveTime::parse_from_str(&time, "%T%.f").unwrap()
+        if let Some(connection) = parse_traffic(&param) {
+            traffic.push(connection);
         }
-        else {
-            Local::now().time()
-        };
-        
-        // println!("{}", naive_time);
-
-
-        //get protocal
-    
-        // let protocal = if let Some(protocol_string) = parts.next() {
-        //     switch
-        // }
-        //
-        let protocal = match parts.next() {
-            Some("IP") => Protocal::Ip,
-            Some("IP6") => Protocal::Ip6,
-            Some("ARP,") => Protocal::Arp,
-            Some("STP") => Protocal::Stp,
-            None => continue,
-            Some(e) => {
-                // dbg!(e);
-                continue;
-            },
-        };
-
-
-        // dbg!(protocal);
-
     }
 
-    // let time = "20:58:26.765637".to_owned();
-
-    // let naive_time = NaiveTime::parse_from_str(&time, "%T%.f").unwrap();
-
-
-
+    for t in traffic {
+        println!("{}", t);
+    }
 }
 
+fn parse_traffic(params: &str) -> Option<Connection> {
+    let parts = params.split(" ").collect::<Vec<_>>();
 
+    //continues if the string input is not a part of the ip or ip6 protocal
+    if let Some(protocal) = parts.get(1) {
+        match protocal {
+            &"IP" | &"IP6" => (),
+            _ => return None,
+        };
+    }
 
+    let timestamp = match NaiveTime::parse_from_str(parts[0], "%T%.f") {
+        Ok(time) => time,
+        Err(_) => return None,
+    };
 
-struct Connection {
-    timestamp: NaiveTime,
-    protocal: Protocal,
-    source: IpAddr,
-    destinatin: IpAddr,
-    packet_type: PacketType,
-    length: i32
+    
+    let (source, dest) = parse_ip_and_ip6(parts[2], parts[4]);
+    return Some(Connection::new(timestamp, source, dest, Vec::new()));
 }
+
+fn parse_ip_and_ip6(source_string: &str, destination_string: &str) -> (Option<SocketAddr>, Option<SocketAddr>){
+    let source = if let Some(port_location) = source_string.rfind('.') {
+        
+        let str_addr = format!("{}:{}", &source_string[..port_location], &source_string[(port_location + 1)..]);
+        SocketAddr::from_str(&str_addr).ok()
+    }
+    else {
+
+        let sock_addr = match IpAddr::from_str(&source_string) {
+            Ok(str_addr) => Some(SocketAddr::new(str_addr, 0)),
+            Err(_) => None,
+        };
+            
+        sock_addr
+    };
+
+
+    let destination_string = destination_string.trim_end_matches(":");
+    let destination = if let Some(port_location) = destination_string.rfind('.') {
+        let str_addr = format!("{}:{}", &destination_string[..port_location], &destination_string[(port_location + 1)..]);
+        SocketAddr::from_str(&str_addr).ok()
+    }
+    else {
+        let sock_addr = match IpAddr::from_str(&destination_string) {
+            Ok(str_addr) => Some(SocketAddr::new(str_addr, 0)),
+            Err(_) => None,
+        };
+
+        sock_addr
+    };
+
+    return (source, destination);
+}
+
 
 #[derive(Debug)]
-enum Protocal {
-    Ip,
-    Ip6,
-    Arp,
-    Stp,
-}
-
-enum Flags {
-    Syn,
-    Fin,
-    Push,
-    Rst
-}
-
-enum PacketType {
-    Tcp{
-        flag: Flags,
-        sequence: (i32, i32),
-        ack: i32,
-        win: i32,
-        options: Vec<String>,
-    },
-    Udp,
+struct Connection {
+    timestamp: NaiveTime,
+    source: Option<SocketAddr>,
+    destination: Option<SocketAddr>,
+    misc: Vec<String>,
 }
 
 
 
+impl Connection {
+    pub fn new(timestamp: NaiveTime, source: Option<SocketAddr>, destination: Option<SocketAddr>, misc: Vec<String>) -> Self {
+        Self {
+            timestamp,
+            source,
+            destination,
+            misc
+        }
+    }
+}
+
+
+
+impl fmt::Display for Connection {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Timestamp: {}\n", self.timestamp)?;
+
+        match &self.source {
+            Some(source) => write!(f, "Source: {}\n", source)?,
+            None => (),
+        };
+
+        match &self.destination {
+            Some(destination) => write!(f, "Destination: {}\n", destination)?,
+            None => (),
+        };
+
+        if !(&self.misc.is_empty()) {
+
+            write!(f, "Misc: {}", self.misc.join(", "))
+        }
+        else {
+            write!(f, "")
+        }
+        
+    }
+}
